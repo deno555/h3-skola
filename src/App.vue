b83e70270195324ab4f9ef37928f777b458af367
Camko25 <samuel.sladky@gmail.com>
2022-10-20 13:44:12 +0200

    rateSubject

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,15 @@
 <template>
-<LunchRating></LunchRating>
-
+  <
 
 </template>
 
 <script>
-import LunchRating from './components/lunchRating.vue';
+
 
 export default{
   components:{
-    LunchRating
-}
+    
+  }
 }
 
 </script>
