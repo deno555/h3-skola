dea08f2bfdec8cff4095051d97b38052262161f9
Camko25 <samuel.sladky@gmail.com>
2022-10-25 10:52:27 +0200

    added router to login and register

--- src/components/register.vue
+++ src/components/register.vue
@@ -12,7 +12,7 @@
                 <input type="text" placeholder="Potvrďte heslo" class=" p-3 w-[440px] rounded-3xl h-12 mt-3 mb-8">
                      <br>
             <a class="border-green-700 rounded-2xl bg-green-700 text-slate-500 px-8 py-3 text-gray-700">Registrovať</a>
-        <h4 class="text-gray-500 mt-5">Už máš účet? Prihlás sa <a class="text-green-700"> tu</a>!</h4>
+        <h4 class="text-gray-500 mt-5">Už máš účet? Prihlás sa <router-link to="/login"><a class="text-green-700"> tu</a></router-link>!</h4>
     </div>
 </div>
 </template>
