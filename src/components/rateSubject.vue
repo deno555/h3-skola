970f4aee8e18146c25a6591f90002b08134f6434
deno555 <gaaldenis@gmail.com>
2022-11-03 13:07:37 +0100

    stars

--- src/components/rateSubject.vue
+++ src/components/rateSubject.vue
@@ -9,27 +9,15 @@
     <div class="text-white text-3xl grid justify-items-center mt-[-60px]">
         <div class="flex m-10">
             <h1 >Baví ma predmet:</h1>
-            <!--input type="checkbox" id="star1"><label for="star1"--><img class="h-8 ml-5" @click="changeStar()" :src="src"><!--/label-->
-            <input type="checkbox" id="star2"><label for="star2"><img class="h-8 ml-5" src="@/assets/images/fullStar.png"></label>
-            <input type="checkbox" id="star3"><label for="star3"><img class="h-8 ml-5" src="@/assets/images/fullStar.png"></label>
-            <input type="checkbox" id="star4"><label for="star4"><img class="h-8 ml-5" src="@/assets/images/fullStar.png"></label>
-            <input type="checkbox" id="star5"><label for="star5"><img class="h-8 ml-5" src="@/assets/images/emptyStar.png"></label>
+            <img class="h-8 ml-5" @click="changeStar()" :src="src" v-for="n in 5" :key="n">
         </div>
         <div class="flex m-10">
             <h1 >Prístup učiteľa:</h1>
-            <input type="checkbox" id="star6"><label for="star6"><img class="h-8 ml-5" src="@/assets/images/fullStar.png"></label>
-            <input type="checkbox" id="star7"><label for="star7"><img class="h-8 ml-5" src="@/assets/images/fullStar.png"></label>
-            <input type="checkbox" id="star8"><label for="star8"><img class="h-8 ml-5" src="@/assets/images/fullStar.png"></label>
-            <input type="checkbox" id="star9"><label for="star9"><img class="h-8 ml-5" src="@/assets/images/fullStar.png"></label>
-            <input type="checkbox" id="star10"><label for="star10"><img class="h-8 ml-5" src="@/assets/images/emptyStar.png"></label>
+            <img class="h-8 ml-5" @click="changeStar()" :src="src" v-for="n in 5" :key="n">
         </div>
         <div class="flex m-10">
             <h1 >Naučil som sa:</h1>
-            <input type="checkbox" id="star11"><label for="star11"><img class="h-8 ml-5" src="@/assets/images/fullStar.png"></label>
-            <input type="checkbox" id="star12"><label for="star12"><img class="h-8 ml-5" src="@/assets/images/fullStar.png"></label>
-            <input type="checkbox" id="star13"><label for="star13"><img class="h-8 ml-5" src="@/assets/images/fullStar.png"></label>
-            <input type="checkbox" id="star14"><label for="star14"><img class="h-8 ml-5" src="@/assets/images/fullStar.png"></label>
-            <input type="checkbox" id="star15"><label for="star15"><img class="h-8 ml-5" src="@/assets/images/emptyStar.png"></label>
+            <img class="h-8 ml-5" @click="changeStar()" :src="src" v-for="n in 5" :key="n">
         </div>          
     </div>
     <div class="grid justify-items-center py-10">
@@ -48,9 +36,7 @@ export default{
     data(){
         return{
             checked:false,
-            count:1,
             src:"https://cdn.discordapp.com/attachments/768503373363806218/1037355367417520138/fullStar.png",
-            number:5,
         }
     },
 
