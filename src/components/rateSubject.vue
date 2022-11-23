f3ab39d4efe16573c913bdf8a5b4c580e07f6d27
deno555 <gaaldenis@gmail.com>
2022-11-23 11:13:01 +0100

    stash

--- src/components/rateSubject.vue
+++ src/components/rateSubject.vue
@@ -9,16 +9,16 @@
     <div class="text-white text-3xl grid justify-items-center mt-[-60px]">
         <div class="flex m-10">
             <h1 >Baví ma predmet:</h1>
-            <img class="h-8 ml-5" @click="changeStar()" :src="src" v-for="n in 5" :key="n">
+            <starRating/>
         </div>
         <div class="flex m-10">
             <h1 >Prístup učiteľa:</h1>
-            <img class="h-8 ml-5" @click="changeStar()" :src="src" v-for="n in 5" :key="n">
+            <starRating/>
         </div>
         <div class="flex m-10">
             <h1 >Naučil som sa:</h1>
-            <img class="h-8 ml-5" @click="changeStar()" :src="src" v-for="n in 5" :key="n">
-        </div>          
+            <starRating/>
+        </div> 
     </div>
     <div class="grid justify-items-center py-10">
         <button class="text-2xl text-white border-2 rounded-[5px] border-lime-900 bg-lime-900 pl-20 pr-20 pt-2 pb-2">Ohodnotiť</button>
@@ -28,20 +28,28 @@
 
 <script>
 import mainHeader from './mainHeader.vue';
+import starRating from './starRating.vue';
 
 export default{
     components:{
         mainHeader,
+        starRating,
     },
     data(){
         return{
             checked:false,
             src:"https://cdn.discordapp.com/attachments/768503373363806218/1037355367417520138/fullStar.png",
+            starID:{
+                star1:1,
+                star2:2,
+                star3:3,
+                star4:4,
+                star5:5}
         }
     },
 
     methods:{
-        changeStar(){
+        changeStarState(){
             if(this.checked==true){
                 this.src="https://cdn.discordapp.com/attachments/768503373363806218/1037355367417520138/fullStar.png"
                 this.checked=false
