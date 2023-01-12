1a4e00690f4b5416a0042c58107bbb01280c7e8a
pietro445 <pepe.polak2004@gmail.com>
2023-01-12 14:08:51 +0100

    css fix

--- src/components/rateSubject.vue
+++ src/components/rateSubject.vue
@@ -1,12 +1,13 @@
 <template>    
 <mainHeader/>
 <div class="bg-zinc-800 flex justify-center pt-20 ">
-
-    <div class="grid justify-items-center py-10">
+    
+    <div class="flex justify-center py-10">
         <img src="@/assets/images/stur.jpg">
-        <h1 class="text-white text-3xl py-10">SJL</h1>
+        <h1 class="text-white text-center text-3xl py-10">SJL</h1>
     </div>
-    <div class="text-white text-3xl grid justify-items-center mt-[-60px]">
+
+    <div class="grid text-white text-3xl justify-items-center mt-[-60px]">
         <div class="flex m-10">
             <h1 >Baví ma predmet:</h1>
             <starRating/>
@@ -23,11 +24,12 @@
             <h1 >Zaujimavy vyklad:</h1>
             <starRating/>
         </div> 
-        
     </div>
-    <div class="grid justify-items-center py-10">
-        <button class="text-2xl text-white border-2 rounded-[5px] border-lime-900 bg-lime-900 pl-20 pr-20 pt-2 pb-2">Ohodnotiť</button>
+
+    <div class="flex justify-center py-10">
+        <button class="h-[80px] text-2xl text-white border-2 rounded-[5px] border-lime-900 bg-lime-900 pl-20 pr-20 pt-2 pb-2">Ohodnotiť</button>
     </div>
+
 </div>
 </template>
 
