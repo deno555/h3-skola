d984e2ba7b80899802e06ee167ed9f401e1dbe36
pietro445 <pepe.polak2004@gmail.com>
2023-01-18 14:25:46 +0100

    commit hh

--- src/components/rateSubject.vue
+++ src/components/rateSubject.vue
@@ -1,32 +1,32 @@
 <template>    
 <mainHeader/>
-<div class="bg-zinc-800 flex justify-center pt-20 ">
+<div class="bg-zinc-800 justify-center pt-20 ">
     
-    <div class="flex justify-center py-10">
-        <img src="@/assets/images/stur.jpg">
-        <h1 class="text-white text-center text-3xl py-10">SJL</h1>
+    <div class="py-10">
+        <img class="mr-auto mt-[-50px] ml-auto" src="@/assets/images/stur.jpg">
+        <h2 class="text-white text-center text-3xl py-5">SJL</h2>
     </div>
 
     <div class="grid text-white text-3xl justify-items-center mt-[-60px]">
-        <div class="flex m-10">
+        <div class="flex pt-5 m-6">
             <h1 >Baví ma predmet:</h1>
             <starRating/>
         </div>
-        <div class="flex m-10">
+        <div class="flex m-6">
             <h1 >Prístup učiteľa:</h1>
             <starRating/>
         </div>
-        <div class="flex m-10">
+        <div class="flex m-6">
             <h1 >Naučil som sa:</h1>
             <starRating/>
         </div> 
-        <div class="flex m-10">
+        <div class="flex m-6">
             <h1 >Zaujimavy vyklad:</h1>
             <starRating/>
         </div> 
     </div>
 
-    <div class="flex justify-center py-10">
+    <div class="flex justify-center pt-10 pb-12">
         <button class="h-[80px] text-2xl text-white border-2 rounded-[5px] border-lime-900 bg-lime-900 pl-20 pr-20 pt-2 pb-2">Ohodnotiť</button>
     </div>
 
