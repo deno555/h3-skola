110e8c02443fc46f3e81a12ec90f6b7affabc6e7
pietro445 <pepe.polak2004@gmail.com>
2023-02-09 13:19:01 +0100

    aaa

--- src/components/lunchRating.vue
+++ src/components/lunchRating.vue
@@ -1,49 +1,42 @@
 <template>
     <mainHeader/>
-    <div class="bg-zinc-800 flex justify-center pt-20">
-        <div class="grid justify-items-center py-10">
-            <img src="@/assets/images/obedPic.jpg">
-            <h1 class="text-white text-3xl py-10">Obed</h1>
+<div class="bg-zinc-800 justify-center pt-20 ">
+    
+    <div class="py-10">
+        <img class="mr-auto mt-[-50px] ml-auto" src="@/assets/images/obedPic.jpg">
+        <h2 class="text-white text-center text-3xl py-5">Obed</h2>
+    </div>
+
+    <div class="grid text-white text-3xl justify-items-center mt-[-60px]">
+        <div class="flex pt-5 m-6">
+            <h1 >Chuť:</h1>
+            <starRating/>
         </div>
-        <div class="text-white text-3xl grid justify-items-center mt-[-60px]">
-            <div class="flex m-10">
-                <h1 >Chuť:</h1>
-                <img class="h-8 ml-5" src="@/assets/images/fullStar.png">
-                <img class="h-8 ml-5" src="@/assets/images/fullStar.png">
-                <img class="h-8 ml-5" src="@/assets/images/fullStar.png">
-                <img class="h-8 ml-5" src="@/assets/images/fullStar.png">
-                <img class="h-8 ml-5" src="@/assets/images/emptyStar.png">
-            </div>
-            <div class="flex m-10">
-                <h1 >Čistota:</h1>
-                <img class="h-8 ml-5" src="@/assets/images/fullStar.png">
-                <img class="h-8 ml-5" src="@/assets/images/fullStar.png">
-                <img class="h-8 ml-5" src="@/assets/images/fullStar.png">
-                <img class="h-8 ml-5" src="@/assets/images/fullStar.png">
-                <img class="h-8 ml-5" src="@/assets/images/emptyStar.png">
-            </div>
-            <div class="flex m-10">
-                <h1 >Variácia:</h1>
-                <img class="h-8 ml-5" src="@/assets/images/fullStar.png">
-                <img class="h-8 ml-5" src="@/assets/images/fullStar.png">
-                <img class="h-8 ml-5" src="@/assets/images/fullStar.png">
-                <img class="h-8 ml-5" src="@/assets/images/fullStar.png">
-                <img class="h-8 ml-5" src="@/assets/images/emptyStar.png">
-            </div>
-          
+        <div class="flex m-6">
+            <h1 >Čistota:</h1>
+            <starRating/>
         </div>
-        <div class="grid justify-items-center py-10">
-            <button class="text-2xl text-white border-2 rounded-[5px] border-lime-900 bg-lime-900 pl-20 pr-20 pt-2 pb-2">Ohodnotiť</button>
-        </div>  
-    </div>  
+        <div class="flex m-6">
+            <h1 >Variácia:</h1>
+            <starRating/>
+        </div> 
+    </div>
+
+    <div class="flex justify-center pt-10 pb-12">
+        <button class="h-[80px] text-2xl text-white border-2 rounded-[5px] border-lime-900 bg-lime-900 pl-20 pr-20 pt-2 pb-2">Ohodnotiť</button>
+    </div>
+
+</div>  
 </template>
 
 <script>
 import mainHeader from './mainHeader.vue';
+import starRating from './starRating.vue';
 
 export default{
     components:{
         mainHeader,
+        starRating,
     },
 data(){
     return{
